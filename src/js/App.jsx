91cab7f35cade2715282c5206acfdd7da6e14209
BmLatin0 <73851025+BMA1656@users.noreply.github.com>
2023-09-22 10:10:14 -0600

    aca tienen sus cards cabrones

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,12 +1,43 @@
 import { createRoot } from "react-dom/client";
+import React from 'react';
+import { Card } from './components/card.jsx';
 
-const App = () => {
+function App() {
+  const cardData = {
+    title: 'Título del evento',
+    image: '../assets/music-001_w50seu.jpg',
+    date: '2023-09-22',
+    address: '123 Calle Principal',
+    city: 'Ciudad',
+    state: 'Estado',
+    price: '$20',
+    interaction: 'going',
+    id: '1',
+    category: 'Evento',
+  };
+  const cardData2 = {
+    title: 'Concierto en Vivo',
+    image: '../assets/concert-001.jpg',
+    date: '2023-10-15',
+    address: '456 Calle Secundaria',
+    city: 'Otra Ciudad',
+    state: 'Otro Estado',
+    price: '$25',
+    interaction: 'interested',
+    id: '2',
+    category: 'Concierto',
+  };
+
   return (
-    <div>
-      Hola lola!
+    <div className="App">
+      <ol id='gallery' className="container error-container gallery home-gallery">
+        <Card {...cardData} />
+        <Card {...cardData2} />
+      </ol>
     </div>
   );
-};
+}
+
 
 const container = document.getElementById("main-gallery");
 const root = createRoot(container);
---
+++ src/js/components/card.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import formatDate from '../utils/format-date.js';
+import { interactionsCategories } from '../config.js'; 
+
+const { going, interested, favorites } = interactionsCategories; 
+
+const Card = ({ title, image, date, address, city, state, price, interaction, id, category }) => {
+  return (
+      <li className="gallery__card">
+        <img src={image} alt={title} />
+        <div className="gallery__text">
+          <div className="event__info">
+            <h3>{title}</h3>
+            <p className="date">{formatDate(new Date(date))}.</p>
+            <p>
+              {address} • {city}, {state}.
+            </p>
+            <strong>{price}</strong>
+          </div>
+          <div className="interactions-container" data-id={id}>
+            <div className="going-and-interested" data-id={id}>
+              <button className="going" data-id={id} data-interaction={going} data-template={going}>
+                Going!
+              </button>
+              <button className="interested" data-id={id} data-interaction={interested} data-template={interested} >
+                Interested
+              </button>
+              <button className="heart" data-id={id} data-interaction={favorites}></button>
+            </div>
+          </div>
+        </div>
+      </li>
+  );
+};
+
+export { Card };
